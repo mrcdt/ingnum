75f413aa1eee66ac714da1a23d911da915b91b7c
Marine <98706914+mrcdt@users.noreply.github.com>
2026-01-14 09:52:18 +0100

    Refactor BonjourController to RentalController

--- RentalService/src/main/java/com/ingnum/rentalservice/controller/BonjourController.java
+++ RentalService/src/main/java/com/ingnum/rentalservice/controller/BonjourController.java
@@ -1,13 +1,39 @@
 package com.ingnum.rentalservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class BonjourController {
+public class RentalController {
+
+    @Value("${customer.service.url}")
+    private String customerServiceUrl;
+
+    Logger logger = LoggerFactory.getLogger(RentalController.class);
 
     @GetMapping("/bonjour")
     public String bonjour() {
-        return "bonjour";
+        return "bonjour de RentalService";
+    }
+
+    @GetMapping("/customer/{name}")
+    public String getCustomer(@PathVariable String name) {
+        RestTemplate restTemplate = new RestTemplate();
+        
+        String url = customerServiceUrl; 
+        
+        logger.info("Envoi de la requête à : " + url);
+        
+        try {
+            String response = restTemplate.getForObject(url, String.class);
+            return "Client : " + name + " | Réponse PHP : " + response;
+        } catch (Exception e) {
+            return "Erreur : le service PHP n'a pas répondu à l'adresse " + url;
+        }
     }
 }
